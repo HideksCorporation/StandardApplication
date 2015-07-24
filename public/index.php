48b42ff3d486ff8bc1e7e5b69dcd49734e73be4a
Hideks <hideksys@gmail.com>
2015-07-23 21:35:33 -0300

    Update public/index.php

--- public/index.php
+++ public/index.php
@@ -1,12 +1,21 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
-$filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 
 if (php_sapi_name() === 'cli-server' && is_file($filename)) {
     return false;
 }
+
+defined('DS')
+    || define('DS', DIRECTORY_SEPARATOR);
+
+defined('APP_DIR')
+    || define('APP_DIR', dirname(__DIR__).DS.'application');
+
+defined('PUB_DIR')
+    || define('PUB_DIR', dirname(__DIR__).DS.'public');
 
 $application = new Hideks\Application;
 $application->run();
